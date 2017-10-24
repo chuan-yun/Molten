770a9f8e71f2897bfbfe6a2b1d8a05d96192f9f9
silkCut <silkcutbeta@gmail.com>
2017-10-24 09:33:28 +0800

    add config

--- .travis/run-tests.sh
+++ .travis/run-tests.sh
@@ -3,7 +3,7 @@ set -ev
 MO_PHP_BIN=$1
 MO_PHP_VERSION=$2
 
-if [ "$MO_PHP_VERSION" -eq "5.3.27" ] 
+if [ "$MO_PHP_VERSION"x  == "5.3.27"x ] 
 then
     REPORT_EXIT_STATUS=1 TEST_PHP_EXECUTABLE=${MO_PHP_BIN} ${MO_PHP_BIN} -n run-tests.php -q -n -d extension=molten.so -d extension=redis.so -d extension=memcached.so --show-diff
 else
